1f87fb379be46acb6435f405835ba129280a7e3e
PedroR-sptech <pedro.silva048@sptech.school>
2024-11-26 21:10:44 -0300

    KPI finalizada

---
+++ Projeto Individual/web-data-viz/src/controllers/kpi1Controller.js
@@ -0,0 +1,19 @@
+var kp1Model = require("../models/kp1Model");
+
+function listar(req, res) {
+    kp1Model.listar().then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar os avisos: ", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+module.exports = {
+    listar
+}
